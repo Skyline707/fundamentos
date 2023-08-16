0c3f68167fda2375c5a666980aec3c0d7cc1488f
SN4KEBLACK <mretamozo@proton.me>
2023-08-16 18:45:07 +0200

    Segunda Clase

---
+++ js/main.js
@@ -0,0 +1,36 @@
+/*
+for(var i = 5; i <= 20; i++) {
+  if(i % 2 == 0) {
+    console.log(i);
+  }
+}
+
+for(var num = 0; num <= 30; num++) {
+  if(num % 2 != 0)
+    console.log(num);
+}
+
+for (var num = 50; num >= 1; num--) {
+  if (num % 10 == 0) {
+    console.log(num + ' - multiplo de 10')
+  } else if (num % 2 != 0) {
+    console.log(num + ' - impar')
+  } else {
+    console.log(num + ' - par')
+  }
+
+}*/
+
+
+var num = 6;
+const constante = 'hola';
+
+var palabra = 'patri';
+
+var animales = ['perro', 'gato', 'pajaro'];
+
+for (var i = 0; i < animales.length; i++){
+  if(animales[i][0] == 'p') {
+     console.log(animales[i]);
+  }
+}
