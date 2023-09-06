cd223383e284031404a92677e59571b3d1cfbf5c
SN4KEBLACK <mretamozo@proton.me>
2023-09-06 10:43:19 +0200

    Ejercicios basicos

---
+++ Ejercicios/basicos.js
@@ -0,0 +1,70 @@
+/*-----------------------EJERCICIOS BASICOS-----------------------*/
+  /*---------------------ESTRUCTURAS DE CONTROL---------------------*/
+  // let: let variable = valor;
+  // arrays: let array = [1, 2, 3, 4, 5]; / vacio: let array = [];
+  // if/else: if (condicion) {} else {}
+  // switch: switch (variable) { case valor: break; default: break; }
+  // for: for (let i = 0; i < ciclos; i++) {}
+  // while: while (condicion) {}
+  // funciones: function nombreFuncion(parametros) {}
+  // console.log: console.log(variable);
+  /*----------------------------------------------------------------*/
+
+
+/*
+* 1. Crear una función que reciba dos números y devuelva el mayor.
+**/
+
+
+
+/*
+* 2. Crear una función que reciba un número y devuelva true si es par y false si es impar.
+**/
+
+
+
+/*
+* 3. Crear una función que reciba un número y devuelva el siguiente número par.
+**/
+
+
+
+/*
+* 4. Crear una función que reciba una edad y devuelva la cantidad de días que esa persona ha vivido. Puedes obviar los años bisiestos.
+**/
+
+
+
+/*
+* 5. Crear una función que reciba una edad y calcule la cantidad de años que le quedan hasta la edad máxima que establezca.
+**/
+
+
+
+/*
+* 6. Crear una función que reciba un array de números y devuelva su media.
+**/
+
+
+
+/*
+* 7. Crear una función que reciba un array de números y devuelva el número mas alto.
+**/
+
+
+
+/*
+* 8. Crea una funcion que reciba un array y un número. La función deberá devolver el número de veces que está repetido ese número en el array.
+**/
+
+
+
+/*
+* 9. Crear una función que reciba un array de strings y devuelva la concatenación de todas ellas.
+**/
+
+
+
+/*
+* 10. Crear una función que reciba un string y lo devuelva al revés.
+**/
